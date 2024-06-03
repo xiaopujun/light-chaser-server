8ba4701056fbcb382765e3cb1331c01fda7f2fbf
dagu <1182810784@qq.com>
2024-06-03 14:15:53 +0800

    调整静态资源映射，统一映射到/static开头

--- src/main/java/com/dagu/lightchaser/config/StaticSourceInterceptor.java
+++ src/main/java/com/dagu/lightchaser/config/StaticSourceInterceptor.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class ImageUrlInterceptor extends WebMvcConfigurationSupport {
+public class StaticSourceInterceptor extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         String projectResourcePath = "file:" + GlobalVariables.PROJECT_RESOURCE_PATH;
-        registry.addResourceHandler("/images/**").addResourceLocations(projectResourcePath + GlobalVariables.SOURCE_IMAGE_PATH);
-        registry.addResourceHandler("/covers/**").addResourceLocations(projectResourcePath + GlobalVariables.COVER_PATH);
+        registry.addResourceHandler("/static/images/**").addResourceLocations(projectResourcePath + GlobalVariables.SOURCE_IMAGE_PATH);
+        registry.addResourceHandler("/static/covers/**").addResourceLocations(projectResourcePath + GlobalVariables.COVER_PATH);
         super.addResourceHandlers(registry);
     }
 
